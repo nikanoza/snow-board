a11a6ad88aa8c3346fa0a7b1595e5477873aabfe
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-30 00:58:46 +0400

    add delay time for restart

--- Assets/scripts/crash.cs
+++ Assets/scripts/crash.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class crash : MonoBehaviour
 {
+    [SerializeField] float loadTime = 2f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "ground")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", loadTime);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/scripts/finishLine.cs
+++ Assets/scripts/finishLine.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class finishLine : MonoBehaviour
 {
+    [SerializeField] float loadTime = 2f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", loadTime);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
