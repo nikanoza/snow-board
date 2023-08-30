09661c6346be010f9920841a814ce464a318742c
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-30 13:19:56 +0400

    add effects triger

--- Assets/scripts/crash.cs
+++ Assets/scripts/crash.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class crash : MonoBehaviour
 {
     [SerializeField] float loadTime = 2f;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadTime);
         }
     }
--- Assets/scripts/finishLine.cs
+++ Assets/scripts/finishLine.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class finishLine : MonoBehaviour
 {
     [SerializeField] float loadTime = 2f;
+    [SerializeField] ParticleSystem finishEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", loadTime);
         }
     }
